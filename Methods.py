833ae8083a55f834744365ba702b17946238a27f
vanloicao <38426858+vanloicao@users.noreply.github.com>
2019-02-20 15:14:22 +0700

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -11,21 +11,6 @@ from sklearn import svm
 from ProcessingData import normalize_data
 from LOF import LocalOutlierFactor
 
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 28 09:41:21 2016
-
-@author: VANLOI
-"""
-
-from BaseOneClass import CentroidBasedOneClassClassifier, DensityBasedOneClassClassifier
-from sklearn.metrics import roc_curve, auc
-from sklearn import svm
-from ProcessingData import normalize_data
-from LOF import LocalOutlierFactor
-
 def auc_density(training_set, testing_set, actual, scale):
     """Compute AUC for density-based methods: Centroid, Negative Mean Distances,
     Kernel Density Estimation and One-class Support Vector Machine, and LOF. 
